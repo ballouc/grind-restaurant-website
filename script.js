e2ce1f504e6c8879913b787f1204e5703a00e550
Patrick <pcdiem@verizon.net>
2023-04-16 18:01:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
     var navbar = document.querySelector('.navbar');
     if (scrollPosition > 0) {
-      navbar.style = "display: block; animation-name: slideDown; animation-duration: .25s; animation-fill-mode: forwards; animation-timing-function: ease-in;";
+      navbar.style = "display: block; animation-name: slideDown; animation-duration: .5s; animation-fill-mode: forwards; animation-timing-function: ease-in;";
     } else {
       navbar.style = "display: none;";
     }
